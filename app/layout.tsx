02b2e37e5c8e8f11308da6c0216297aa304dacbb
Nishant Bisht <nishantbisht330@gmail.com.com>
2026-08-09 10:39:05 +0530

    update layout file

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import { ClerkProvider } from "@clerk/nextjs";
 import "./globals.css";
+import SupabaseProvider from "@/lib/supabase/SupabaseProvider";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -30,7 +31,9 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
       >
         <body className="min-h-full flex flex-col">
-        {children}
+          <SupabaseProvider >
+            {children}
+          </SupabaseProvider>
         </body>
       </html>
     </ClerkProvider>
